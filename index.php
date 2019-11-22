1e1e0ff98a04c44a225eea9ecc7215149c1b2c8e
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-11-22 09:36:18 +0100

    Insert

--- index.php
+++ index.php
@@ -5,77 +5,94 @@
 <title>Control bd</title>
 </head>
 <body>
+  <table  border="1">
+    <tr bgcolor="#FF9911">
+      <th>Código</th><th>Descripción</th><th>Precio de<br>compra</th><th>Precio de<br>venta</th><th>Margen</th><th>Stock</th><th colspan="4"></th>
+    </tr>
+    <tr>
+      <?php
+        //Conexión a la base de datos
+        $conexion=new mysqli("localhost","root","","examen");
+        $conexion->set_charset("utf8");
+        //Hacemos la Select SQL
+        $sqlPrincipal="SELECT codigo,descripcion,precio_compra,precio_venta, precio_venta-precio_compra as margen, stock from articulo";
+        //En esta conexion hazme esta consulta:
+        $resultPrincipal=$conexion->query($sqlPrincipal);
+        //Metemos en un array los resultados
+        while ($fila=$resultPrincipal->fetch_assoc())
+        {
+          extract($fila);
+          echo "<td>$codigo</td><td>$descripcion</td><td>$precio_compra</td><td>$precio_venta</td><td>$margen</td><td>$stock</td>";
+          echo "<td><form action='borrar.php' method='post'>
+                    <input id='codigo' name='codigo' type='hidden' value='$codigo'>
+                    <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
+                    <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
+                    <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
+                    <input id='stock' name='stock' type='hidden' value='$stock'>
+                    <button type='submit'>Eliminar</button>
+                  </form></td>
+                  <td><form action='modificar.php' method='post'>
+                  <input id='codigo' name='codigo' type='hidden' value='$codigo'>
+                    <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
+                    <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
+                    <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
+                    <input id='stock' name='stock' type='hidden' value='$stock'>
+                    <input id='codigo' name='codigo' type='hidden' value='$codigo'>
+                    <button type='submit'>Modificar</button>
+                  </form></td>						
+                  <td><form action='entrada.php' method='post'>
+                  <input id='codigo' name='codigo' type='hidden' value='$codigo'>
+                    <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
+                    <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
+                    <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
+                    <input id='stock' name='stock' type='hidden' value='$stock'>
+                    <input id='codigo' name='codigo' type='hidden' value='$codigo'>
+                    <button type='submit'>Entrada</button>
+                  </form></td>						
+                  <td><form action='salida.php' method='post'>
+                  <input id='codigo' name='codigo' type='hidden' value='$codigo'>
+                    <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
+                    <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
+                    <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
+                    <input id='stock' name='stock' type='hidden' value='$stock'>
+                    <input id='codigo' name='codigo' type='hidden' value='$codigo'>
+                    <button type='submit'>Salida</button>
+                </form></td>					
+                </tr>";
+          }
+        ?>
 
-   
-<table  border="1">
-  <tr bgcolor="#FF9911">
-    <th>Código</th><th>Descripción</th><th>Precio de<br>compra</th><th>Precio de<br>venta</th><th>Margen</th><th>Stock</th><th colspan="4"></th>
+  <!-- Añadir un nuevo articulo -->
+
+  	<tr bgcolor="888EEE"><td><b>Código</b></td><td><b>Descripción</b></td><td><b>Precio de compra</b></td><td><b>Precio de venta</b></td><td></td><td><b>Stock</b></td>
+  	<td colspan="4"></td></tr>
+      <form method="post" action="index.php">
+        <td><input type="text" name="codigo" size="10" required></td>
+        <td><input type="text" name="descripcion" required></td>
+        <td><input type="number" min="0" name="precio_compra" step="0.01" style="width: 7em" required></td>
+        <td><input type="number" min="0" name="precio_venta" step="0.01"style="width: 7em" required></td>
+        <td></td>
+        <td><input type="number" min="0" name="stock" style="width: 7em" required></td>
+        <td colspan="4">
+  		    <input type="submit"  name="accion" value="Nuevo articulo">       
+        </td>
+      </form>
   </tr>
-  <tr>
-    <?php
-      //Conexión a la base de datos
-      $conexion=new mysqli("localhost","root","","examen");
-      $conexion->set_charset("utf8");
-      //Hacemos la Select SQL
-      $sqlPrincipal="SELECT codigo,descripcion,precio_compra,precio_venta, precio_venta-precio_compra as margen, stock from articulo";
-      //En esta conexion hazme esta consulta:
-      $resultPrincipal=$conexion->query($sqlPrincipal);
-      //Metemos en un array los resultados
-      while ($fila=$resultPrincipal->fetch_assoc())
-      {
-        extract($fila);
-        echo "<td>$codigo</td><td>$descripcion</td><td>$precio_compra</td><td>$precio_venta</td><td>$margen</td><td>$stock</td>";
-        echo "<td><form action='borrar.php' method='post'>
-                  <input id='codigo' name='codigo' type='hidden' value='$codigo'>
-                  <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
-                  <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
-                  <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
-                  <input id='stock' name='stock' type='hidden' value='$stock'>
-                  <button type='submit'>Eliminar</button>
-                </form></td>
-                <td><form action='modificar.php' method='post'>
-                <input id='codigo' name='codigo' type='hidden' value='$codigo'>
-                  <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
-                  <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
-                  <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
-                  <input id='stock' name='stock' type='hidden' value='$stock'>
-                  <input id='codigo' name='codigo' type='hidden' value='$codigo'>
-                  <button type='submit'>Modificar</button>
-                </form></td>						
-                <td><form action='entrada.php' method='post'>
-                <input id='codigo' name='codigo' type='hidden' value='$codigo'>
-                  <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
-                  <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
-                  <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
-                  <input id='stock' name='stock' type='hidden' value='$stock'>
-                  <input id='codigo' name='codigo' type='hidden' value='$codigo'>
-                  <button type='submit'>Entrada</button>
-                </form></td>						
-                <td><form action='salida.php' method='post'>
-                <input id='codigo' name='codigo' type='hidden' value='$codigo'>
-                  <input id='descripcion' name='descripcion' type='hidden' value='$descripcion'>
-                  <input id='precio_compra' name='precio_compra' type='hidden' value='$precio_compra'>
-                  <input id='precio_venta' name='precio_venta' type='hidden' value='$precio_venta'>
-                  <input id='stock' name='stock' type='hidden' value='$stock'>
-                  <input id='codigo' name='codigo' type='hidden' value='$codigo'>
-                  <button type='submit'>Salida</button>
-              </form></td>					
-              </tr>";
-        }
-      ?>
-
-<!-- Añadir un nuevo articulo -->
-
-	<tr bgcolor="888EEE"><td><b>Código</b></td><td><b>Descripción</b></td><td><b>Precio de compra</b></td><td><b>Precio de venta</b></td><td></td><td><b>Stock</b></td>
-	<td colspan="4"></td></tr>
-      <td><input type="text" name="codigo" size="10"></td>
-      <td><input type="text" name="descripcion"></td>
-      <td><input type="number" min="0" name="precio_compra" step="0.01" style="width: 7em"></td>
-      <td><input type="number" min="0" name="precio_venta" step="0.01"style="width: 7em"></td>
-      <td></td>
-      <td><input type="number" min="0" name="stock" style="width: 7em"></td>
-      <td colspan="4">
-		    <input type="submit"  name="accion" value="Nuevo articulo">       
-      </td>
-</tr>
-</table>
+  </table>
+  <?php
+    if(isset($_POST["accion"])){
+      extract($_POST);
+      $sqlBuscar = "SELECT codigo from articulo where $codigo = codigo";
+      if((int)mysqli_num_rows($conexion->query($sqlBuscar))==0){
+        $sqlNuevo = "INSERT INTO articulo VALUES ('$codigo','$descripcion','$precio_compra','$precio_venta','$stock')";
+        $conexion->query($sqlNuevo);
+        echo '<meta http-equiv="refresh" content="0.1">';
+      }
+      else{
+        echo "<p style='color: #D93C34FF;'>Ya existe un artículo con ese código</p>";
+      }
+    }
+    mysqli_close($conexion);
+  ?>
+</body>
+</html>
