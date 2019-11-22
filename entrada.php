86538164570c9e2e68377b995ea476321da73245
xjmardia <javiermarindz@gmail.com>
2019-11-22 13:51:07 +0100

    soy una patata
    es mentira jeje te lo has creido :laughing:

--- entrada.php
+++ entrada.php
@@ -17,11 +17,32 @@
   <tr><td>Precio de venta: </td><td>$precio_venta</td></tr>
   <tr><td>Stock: </td><td>$stock</td></tr>";
   ?>
-  <tr><td>Unidades que entran al almacén:</td><td><input type="number" min="0"  name="unidades" ></td></tr>
-  <tr><td><input type="submit" name="accion" value="Entrada"><td><input type="submit"  name="accion" value="Cancelar"></td></tr>
-
+  <form method="post">
+      <?php
+        echo "<input type='hidden' name='codigo' value='$codigo'>
+        <input type='hidden' name='stock' value='$stock'>
+        <tr><td>Unidades que entran al almacén: </td><td><input type='number' min='0' name='unidades' ></td></tr>";
+      ?>
+      <tr><td><input type="submit" name="accion" value="Entrada"><td><input type="submit"  name="accion" value="Cancelar"></td></tr>
+  </form>
 </table>
-
+<?php
+    // Comprobamos que se haya pulsado el botón
+    if(isset($_POST["accion"])){
+      // Si el valor del botón que habíamos pulsado, continuamos y extrae del form los valores de los names
+      if($_POST["accion"]=="Entrada"){
+        extract($_POST);
+        $conexion=mysqli_connect ("localhost","root","","examen");
+        $sql = "UPDATE articulo SET stock=stock+$unidades WHERE codigo = '$codigo'";
+        $conexion->query($sql);
+        mysqli_close($conexion);
+        header("Location:index.php");
+      }
+      if($_POST["accion"]=="Cancelar"){
+        header("Location:index.php");
+      }
+    }
+  ?>
 </body>
 </html>
  
